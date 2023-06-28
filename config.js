557ae587d041105f51846e2e085d83c629a66934
NOLTEPRO <114636654+NOLTEPRO@users.noreply.github.com>
2023-06-28 22:44:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['2250708779997', 'GURU', true],
+  ['2250708779997', 'Pro max Brown', true], 
   [''] 
 ] //Number of owners
 
